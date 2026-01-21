fe730ab2d05c3d4fdec96bf27cf0eeec94992c29
GIAN MARCO <79642581+GianMarcoCoppari@users.noreply.github.com>
2026-01-21 11:06:34 +0100

    fully pipelined cla4

--- CLA4.vhd
+++ CLA4.vhd
@@ -1,10 +1,11 @@
 -- Synchronized CLA4
 -- Gian Marco Coppari
--- 2025/11/26
+-- 2026/01/21
 
 
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
 
 
 ENTITY CLA4 IS 
@@ -29,55 +30,47 @@ ARCHITECTURE RTL OF CLA4 IS
     SIGNAL P : STD_LOGIC_VECTOR(3 DOWNTO 0); -- COMBINATORIAL
     SIGNAL G : STD_LOGIC_VECTOR(3 DOWNTO 0); -- COMBINATORIAL
     
-    SIGNAL PREG : STD_LOGIC_VECTOR(3 DOWNTO 0);
+    -- REGISTRI PER RITARDARE LE OPERAZIONI DI UN COLPO DI CLOCK
+    SIGNAL PREG   : STD_LOGIC_VECTOR(3 DOWNTO 0);
+    SIGNAL CINREG : STD_LOGIC;
     
 BEGIN 
-    
     COMPUTEPG : PROCESS (CLK, RST) BEGIN 
         IF RST = '1' THEN 
-            -- Asynchronous Reset Logic Here...
-            G <= (OTHERS => '0');
-            P <= (OTHERS => '0');
+            P      <= (OTHERS => '0');
+            G      <= (OTHERS => '0');
+            CINREG <= '0';
             
         ELSE 
             IF RISING_EDGE(CLK) THEN 
-                -- 1) Compute Propagate & Generate Functions, Fully Parallel
-                P <= A XOR B;
-                G <= A AND B;
+                P      <= A XOR B;
+                G      <= A AND B;
+                CINREG <= CIN;
                 
-            END IF; -- Rising Edge
-        END IF; -- Reset
+            END IF; -- RISING EDGE
+        END IF; -- RST
     END PROCESS COMPUTEPG;
-    
-    COMPUTECARRY : PROCESS (CLK, RST) BEGIN 
-        IF RST = '1' THEN 
-            C <= (OTHERS => '0');            
 
-        ELSE 
-            IF RISING_EDGE(CLK) THEN 
-                -- 2a) Compute Input Carry, Fully Parallel
-                C(0) <= CIN;
-                C(1) <= G(0) OR (P(0) AND CIN);
-                C(2) <= G(1) OR (P(1) AND G(0)) OR (P(1) AND P(0) AND CIN);
-                C(3) <= G(2) OR (P(2) AND G(1)) OR (P(2) AND P(1) AND G(0)) OR (P(2) AND P(1) AND P(0) AND CIN);
-                C(4) <= G(3) OR (P(3) AND G(2)) OR (P(3) AND P(2) AND G(1)) OR (P(3) AND P(2) AND P(1) AND G(0)) OR (P(3) AND P(2) AND P(1) AND P(0) AND CIN);
-                
-            END IF;
-        END IF;
-    END PROCESS COMPUTECARRY;
-    
-    STAGEP : PROCESS (CLK, RST) BEGIN 
+    COMPUTECARRY: PROCESS (CLK, RST) BEGIN 
         IF RST = '1' THEN 
+            C    <= (OTHERS => '0');
             PREG <= (OTHERS => '0');
             
         ELSE 
             IF RISING_EDGE(CLK) THEN 
-                -- 2b) Stage P Signal to Update SUM Only Once
+                -- CALCOLO DEI RIPORTI IN PARALLELO
+                C(0) <= CINREG;
+                C(1) <= G(0) OR (P(0) AND CINREG);
+                C(2) <= G(1) OR (P(1) AND G(0)) OR (P(1) AND P(0) AND CINREG);
+                C(3) <= G(2) OR (P(2) AND G(1)) OR (P(2) AND P(1) AND G(0)) OR (P(2) AND P(1) AND P(0) AND CINREG);
+                C(4) <= G(3) OR (P(3) AND G(2)) OR (P(3) AND P(2) AND G(1)) OR (P(3) AND P(2) AND P(1) AND G(0)) OR (P(3) AND P(2) AND P(1) AND P(0) AND CINREG);
+                
+                -- RITARDO IL SEGNALE P DI UN COLPO DI CLOCK
                 PREG <= P;
                 
-            END IF;
-        END IF;
-    END PROCESS STAGEP;
+            END IF; -- RISING EDGE 
+        END IF; -- RST
+    END PROCESS COMPUTECARRY;
     
     REGOUT : PROCESS (CLK, RST) BEGIN 
         IF RST = '1' THEN 
@@ -87,13 +80,13 @@ BEGIN
             
         ELSE 
             IF RISING_EDGE(CLK) THEN 
-                -- 3) Assign Outputs
-                SUM  <= PREG XOR C(C'HIGH - 1 DOWNTO 0);
-                COUT <= C(C'HIGH);
-                OVF  <= C(C'HIGH) XOR C(C'HIGH - 1);
+                SUM  <= PREG XOR C(3 DOWNTO 0);
+                COUT <= C(4);
+                OVF  <= C(4) XOR C(3);
                 
             END IF;
         END IF;
     END PROCESS REGOUT;
+
 
 END ARCHITECTURE RTL;
