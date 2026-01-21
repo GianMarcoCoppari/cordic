a36707c57982ea467c241c50a310302043798756
GIAN MARCO <79642581+GianMarcoCoppari@users.noreply.github.com>
2026-01-21 12:32:57 +0100

    fixed point representation config file

--- CONFIG.vhd
+++ CONFIG.vhd
@@ -1,31 +1,25 @@
--- Utility Package for VHDL Designs
-
-
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
 
--- TODO: RENDERE PRIVATE COSTANTI CHE NON DEVO USARE ESPLICITAMENTE NEI MODULI
--- TODO: Modulare Configuration Files per Ogni Modulo Indipendente
-PACKAGE CONFIG IS
-    -- Costanti di Parametrizzazione Default Progetto
-    -- Formato Numerico
-    CONSTANT M_BLOCKSIZE : INTEGER := 4;
-    CONSTANT M_BLOCKS    : INTEGER := 9; -- N. di Blocchi di Default, Fixed Point 
-    CONSTANT M_BFRAC     : INTEGER := 7; -- N. di Blocchi per la Parte Frazionaria
+PACKAGE CONFIG IS    
+    CONSTANT M_BLOCKSIZE : INTEGER := 4; -- DIMENSIONE DI UN BLOCCO DI BIT
+    CONSTANT M_BLOCKS    : INTEGER := 9; -- 36 BIT TOTALI
+    CONSTANT M_BFRAC     : INTEGER := 7; -- 28 BIT FRAZIONARI
     
     CONSTANT PERIOD : TIME := 10 ns;
+    CONSTANT M_CLA4LATENCY : INTEGER := 3; -- LATENZA DEL CLA4 IN COLPI DI CLOCK
     
-    
-    -- Tipi e Costanti per il Calcolo della Spline
     TYPE SUM_T IS RECORD 
-        LHS : STD_LOGIC_VECTOR(M_BLOCKSIZE * M_BLOCKS - 1 DOWNTO 0);
-        RHS : STD_LOGIC_VECTOR(M_BLOCKSIZE * M_BLOCKS - 1 DOWNTO 0);
+        A   : STD_LOGIC_VECTOR(M_BLOCKSIZE * M_BLOCKS - 1 DOWNTO 0);
+        B   : STD_LOGIC_VECTOR(M_BLOCKSIZE * M_BLOCKS - 1 DOWNTO 0);
         CIN : STD_LOGIC;
     END RECORD SUM_T;
     
+    
 END PACKAGE CONFIG;
+
 
 PACKAGE BODY CONFIG IS 
 END PACKAGE BODY CONFIG;
